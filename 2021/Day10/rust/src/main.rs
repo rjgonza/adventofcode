95339ee814e29c158594a9bc76a5355b1df26ad5
Ramon Gonzalez <rjgonza@gmail.com>
2021-12-14 21:04:52 -0500

    Adding a solution for the Day10 problems

--- 2021/Day10/rust/src/main.rs
+++ 2021/Day10/rust/src/main.rs
@@ -1,11 +1,80 @@
 static INPUT_FILE: &str = include_str!("../../input.txt");
 
-fn part1(_input: &str) -> usize {
-    todo!();
+fn closing(c: char) -> char {
+    match c {
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
+        _ => panic!("unrecognized character {}", c),
+    }
 }
 
-fn part2(_input: &str) -> usize {
-    todo!();
+fn part1(input: &str) -> usize {
+    let mut score = 0;
+
+    'outer: for line in input.lines() {
+        let mut chunck_chars: Vec<char> = Vec::new();
+        for value in line.trim().chars() {
+            match value {
+                '(' | '[' | '{' | '<' => chunck_chars.push(value),
+                ')' | ']' | '}' | '>' => {
+                    if chunck_chars.is_empty() || value != closing(*chunck_chars.last().unwrap()) {
+                        score += match value {
+                            ')' => 3,
+                            ']' => 57,
+                            '}' => 1197,
+                            '>' => 25137,
+                            _ => panic!("Unable to parse {}", value),
+                        };
+                        continue 'outer;
+                    }
+                    chunck_chars.pop();
+                }
+                _ => panic!("Unable to parse {}", value),
+            }
+        }
+    }
+
+    score
+}
+
+fn part2(input: &str) -> isize {
+    let mut score_list: Vec<isize> = Vec::new();
+
+    'outer: for line in input.lines() {
+        let mut chunck_chars: Vec<char> = Vec::new();
+        for value in line.trim().chars() {
+            match value {
+                '(' | '[' | '{' | '<' => chunck_chars.push(value),
+                ')' | ']' | '}' | '>' => {
+                    if chunck_chars.is_empty() || value != closing(*chunck_chars.last().unwrap()) {
+                        continue 'outer;
+                    }
+                    chunck_chars.pop();
+                }
+                _ => panic!("Unable to parse {}", value),
+            }
+        }
+
+        chunck_chars.reverse();
+        let mut score = 0;
+        for value in chunck_chars {
+            score *= 5;
+            score += match closing(value) {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => panic!("Unable to parse {}", value),
+            };
+        }
+        score_list.push(score);
+    }
+
+    score_list.sort_unstable();
+    let mid = score_list.len() / 2;
+    score_list[mid]
 }
 
 fn main() {
@@ -17,16 +86,24 @@ fn main() {
 
 mod test {
     use super::*;
-    const INPUT: &str = "";
+    const INPUT: &str = "[({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]";
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(INPUT), 0);
+        assert_eq!(part1(INPUT), 26397);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT), 0);
+        assert_eq!(part2(INPUT), 288957);
     }
 }
-
