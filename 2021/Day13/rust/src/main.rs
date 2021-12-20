7e5f6936f71db59b1165e21a822ebefed0d3bdb1
Ramon Gonzalez <rjgonza@gmail.com>
2021-12-19 21:41:24 -0500

    Adding day 13

--- 2021/Day13/rust/src/main.rs
+++ 2021/Day13/rust/src/main.rs
@@ -1,7 +1,25 @@
+use std::collections::HashMap;
+
 static INPUT_FILE: &str = include_str!("../../input.txt");
 
-fn part1(_input: &str) -> usize {
-    todo!();
+fn part1(input: &str) -> usize {
+    let mut coords: Vec<(usize, usize)> = Vec::new();
+    let mut folds: HashMap<&str, usize> = HashMap::new();
+
+    for lines in input.lines() {
+        if lines.contains(',') {
+            let this: (&str, &str) = lines.trim().split_once(',').unwrap();
+            coords.push((this.0.parse().unwrap(), this.1.parse().unwrap()));
+        } else if lines.contains('=') {
+            let data: Vec<&str> = lines.trim().split_whitespace().collect();
+            let (var, val) = data[data.len() - 1].split_once('=').unwrap();
+            folds.insert(var, val.parse().unwrap());
+        }
+    }
+
+    dbg!(coords);
+    dbg!(folds);
+    0
 }
 
 fn part2(_input: &str) -> usize {
@@ -17,15 +35,35 @@ fn main() {
 
 mod test {
     use super::*;
-    const INPUT: &str = "";
+    const INPUT: &str = "6,10
+    0,14
+    9,10
+    0,3
+    10,4
+    4,11
+    6,0
+    6,12
+    4,1
+    0,13
+    10,12
+    3,4
+    3,0
+    8,4
+    1,10
+    2,14
+    8,10
+    9,0
+    
+    fold along y=7
+    fold along x=5";
 
     #[test]
     fn test_part1() {
         assert_eq!(part1(INPUT), 0);
     }
 
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2(INPUT), 0);
-    }
+    // #[test]
+    // fn test_part2() {
+    //     assert_eq!(part2(INPUT), 0);
+    // }
 }
