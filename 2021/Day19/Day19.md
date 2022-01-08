9bbacf353c9f27cac8483fccf59908df24d83377
Ramon Gonzalez <rjgonza@gmail.com>
2022-01-08 08:58:51 -0500

    Syncing up some changes for day 19

--- 2021/Day19/Day19.md
+++ 2021/Day19/Day19.md
@@ -368,4 +368,16 @@ Assemble the full map of beacons. **How many beacons are there?**
 
 To begin, get your puzzle input.
 
+Your puzzle answer was `394`.
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+#--- Part Two ---
+
+Sometimes, it's a good idea to appreciate just how big the ocean is. Using the [Manhattan distance](https://en.wikipedia.org/wiki/Taxicab_geometry), how far apart do the scanners get?
+
+In the above example, scanners `2` (`1105,-1205,1229`) and `3` (`-92,-2380,-20`) are the largest Manhattan distance apart. In total, they are `1197 + 1175 + 1249 = `**`3621`** units apart.
+
+**What is the largest Manhattan distance between any two scanners?**
+
 Answer:
--- 2021/Day19/rust/src/main.rs
+++ 2021/Day19/rust/src/main.rs
@@ -1,48 +1,156 @@
-use std::{collections::HashMap, iter::Scan};
+use std::collections::{HashMap, HashSet};
+use std::ops::{Add, Neg, Sub};
 
 static INPUT_FILE: &str = include_str!("../../input.txt");
-
-#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
-struct Point(isize, isize, isize);
-
-#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
-struct Beacon(Point);
-
-#[derive(Debug)]
-struct Scanner {
-    location: Point,
-    beacons: Vec<Beacon>,
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Vec3 {
+    x: i32,
+    y: i32,
+    z: i32,
 }
 
-impl Scanner {
-    fn add_beacon(&mut self, b: Beacon) {
-        self.beacons.push(b);
+impl Add for Vec3 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
     }
 }
 
-fn parse_input(input: &str) -> Vec<Scanner> {
-    let mut scanners: Vec<Scanner> = Vec::new();
-    let mut scanner_i = 0;
-    for line in input.lines() {
-        if line.is_empty() {
-            continue;
+impl Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
         }
-        if line.contains("---") {
-            scanner_i = line.split_whitespace().nth(2).unwrap().parse().unwrap();
-            // println!("Scanner! - {}", scanner);
-            scanners.push(Scanner {
-                location: (Point(0, 0, 0)),
-                beacons: Vec::new(),
-            });
-            continue;
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Vec3 {
+    fn zero() -> Self {
+        Self { x: 0, y: 0, z: 0 }
+    }
+
+    fn rot(self, axis_rotations: Self) -> Self {
+        let mut res = self;
+
+        // x-axis rotation
+        for _ in 0..axis_rotations.x {
+            let prev_z = res.z;
+            res.z = res.y;
+            res.y = -prev_z;
         }
 
-        let coord: Vec<isize> = line.split(',').map(|c| c.parse().unwrap()).collect();
-        scanners[scanner_i].add_beacon(Beacon(Point(coord[0], coord[1], coord[2])));
+        // y-axis rotation
+        for _ in 0..axis_rotations.y {
+            let prev_z = res.z;
+            res.z = -res.x;
+            res.x = prev_z;
+        }
+
+        // z-axis rotation
+        for _ in 0..axis_rotations.z {
+            let prev_y = res.y;
+            res.y = res.x;
+            res.x = -prev_y;
+        }
+
+        res
     }
-    scanners
-    // dbg!(scanners);
 }
+
+fn translate_points(src: &HashSet<Vec3>, translate: Vec3) -> HashSet<Vec3> {
+    src.iter().map(|p| *p + translate).collect()
+}
+
+fn find_offset(a: &HashSet<Vec3>, b: &HashSet<Vec3>, min_similar: usize) -> Option<Vec3> {
+    for a_origin in a.iter() {
+        let a_translated = translate_points(a, -*a_origin);
+        for b_origin in b.iter() {
+            let b_translated = translate_points(b, -*b_origin);
+            if a_translated.intersection(&b_translated).count() >= min_similar {
+                return Some(*a_origin - *b_origin);
+            }
+        }
+    }
+
+    None
+}
+
+fn all_rotations() -> Vec<Vec3> {
+    let mut res = Vec::new();
+    let mut known_rots = HashSet::new();
+
+    for xrot in 0..=3 {
+        for yrot in 0..=3 {
+            for zrot in 0..=3 {
+                let rot = Vec3 {
+                    x: xrot,
+                    y: yrot,
+                    z: zrot,
+                };
+                let axes_rotated = (
+                    Vec3 { x: 1, y: 0, z: 0 }.rot(rot),
+                    Vec3 { x: 0, y: 1, z: 0 }.rot(rot),
+                    Vec3 { x: 0, y: 0, z: 1 }.rot(rot),
+                );
+                if known_rots.contains(&axes_rotated) {
+                    continue;
+                }
+
+                known_rots.insert(axes_rotated);
+                res.push(rot);
+            }
+        }
+    }
+
+    res
+}
+
+// fn parse_input(input: &str) -> Vec<Scanner> {
+//     let mut scanners: Vec<Scanner> = Vec::new();
+//     let all_rots = all_rotations();
+//     let mut scanner_i = 0;
+//     for line in input.lines() {
+//         if line.trim().is_empty() {
+//             continue;
+//         }
+//         if line.contains("---") {
+//             scanner_i = line.split_whitespace().nth(2).unwrap().parse().unwrap();
+//             // println!("Scanner! - {}", scanner);
+//             scanners.push(Scanner {
+//                 location: (Point(0, 0, 0)),
+//                 // location: (Point(0, 0)),
+//                 beacons: Vec::new(),
+//             });
+//             continue;
+//         }
+
+//         // dbg!(line);
+
+//         let coord: Vec<isize> = line.trim().split(',').map(|c| c.parse().unwrap()).collect();
+//         scanners[scanner_i].add_beacon(Beacon(Point(coord[0], coord[1], coord[2])));
+//         // scanners[scanner_i].add_beacon(Beacon(Point(coord[0], coord[1])));
+//     }
+//     scanners
+//     // dbg!(scanners);
+// }
 
 fn main() {
     println!("Part 1: {}", part1(INPUT_FILE));
@@ -50,30 +158,88 @@ fn main() {
 }
 
 fn part1(input: &str) -> usize {
-    let mut scanners = parse_input(input);
-    // dbg!(scanners);
+    let mut lines = input.lines().peekable();
+    let mut scanners = Vec::new();
+    let all_rots = all_rotations();
 
-    let max = scanners[0].beacons.iter().max().unwrap();
-    dbg!(max);
-    let x = max.0 .0.abs();
-    let y = max.0 .1.abs();
-    let z = max.0 .2.abs();
+    while lines.peek().is_some() {
+        // consume hyphen
+        lines.next();
 
-    dbg!(x, y, z);
+        let mut points = HashSet::new();
+        loop {
+            if lines.peek().is_none() {
+                break;
+            }
 
-    // let map = vec![vec![vec![' '; x as usize]; y as usize]; z as usize];
-    let map = vec![vec!['.'; x as usize]; y as usize];
-    for row in map {
-        for col in row {
-            print!("{}", col);
+            let line = lines.next().unwrap();
+            if line.len() == 0 {
+                break;
+            }
+
+            let coords: Vec<i32> = line.split(",").map(|toks| toks.parse().unwrap()).collect();
+            points.insert(Vec3 {
+                x: coords[0],
+                y: coords[1],
+                z: coords[2],
+            });
         }
-        println!();
+
+        // Insert the scanner's list of beacon positions, along with a cache of
+        // pre-rotated positions. This allows us to avoid re-processing
+        // rotations during brute-force search.
+        let mut rots: HashMap<Vec3, HashSet<Vec3>> = HashMap::new();
+        for r in all_rots.iter() {
+            rots.insert(*r, points.iter().map(|p| p.rot(*r)).collect());
+        }
+
+        scanners.push(rots);
     }
 
-    // for beacon in scanners[0].beacons.iter() {
-    //     dbg!(beacon);
-    // }
-    79
+    let min_overlap = 12;
+
+    // `resolved` is a map between scanner IDs and a tuple of the scanner's
+    // beacon positions, transformed to scanner 0's space.
+    let mut resolved = HashMap::new();
+    resolved.insert(0, scanners[0][&Vec3::zero()].clone());
+
+    let mut unresolved = HashSet::new();
+    for i in 1..scanners.len() {
+        unresolved.insert(i);
+    }
+
+    let mut unrelated_scanners = HashSet::new();
+
+    'outer: while unresolved.len() > 0 {
+        for a_id in resolved.keys().cloned() {
+            let a_points = &resolved[&a_id];
+
+            for b_id in unresolved.clone() {
+                if unrelated_scanners.contains(&(a_id, b_id)) {
+                    continue;
+                }
+
+                for b_points in scanners[b_id].values() {
+                    if let Some(b_to_a) = find_offset(a_points, b_points, min_overlap) {
+                        unresolved.remove(&b_id);
+                        resolved.insert(b_id, translate_points(b_points, b_to_a));
+                        continue 'outer;
+                    }
+                }
+
+                unrelated_scanners.insert((a_id, b_id));
+            }
+        }
+
+        unreachable!();
+    }
+
+    let mut beacons = HashSet::new();
+    for scanner_beacons in resolved.values() {
+        beacons = beacons.union(scanner_beacons).copied().collect();
+    }
+
+    beacons.len()
 }
 
 fn part2(_input: &str) -> usize {
@@ -84,6 +250,7 @@ fn part2(_input: &str) -> usize {
 
 mod test {
     use super::*;
+
     static TEST_INPUT: &str = include_str!("../../test_input.txt");
 
     #[test]
@@ -91,8 +258,8 @@ mod test {
         assert_eq!(part1(TEST_INPUT), 79);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(TEST_INPUT), 0);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 3621);
+    }
 }
