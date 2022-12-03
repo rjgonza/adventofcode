bb8d8f0beda7de238f82ef8fa460f1036898ae8b
rjgonza <rjgonza@gmail.com>
2022-12-02 20:51:35 -0500

    Saving progress

--- 2022/Day2/rust/src/main.rs
+++ 2022/Day2/rust/src/main.rs
@@ -17,7 +17,26 @@ impl FromStr for RPSChoices {
             "A" | "X" => Ok(RPSChoices::Rock),
             "B" | "Y" => Ok(RPSChoices::Paper),
             "C" | "Z" => Ok(RPSChoices::Scissors),
-            _ => Err("Unknown move".to_string()),
+            _ => Err(s.to_string()),
+        }
+    }
+}
+
+enum Outcomes {
+    Win = 6,
+    Lose = 0,
+    Draw = 3,
+}
+
+impl FromStr for Outcomes {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Y" => Ok(Outcomes::Lose),
+            "X" => Ok(Outcomes::Draw),
+            "Z" => Ok(Outcomes::Win),
+            _ => Err(s.to_string()),
         }
     }
 }
@@ -45,28 +64,53 @@ fn part1(input: &str) -> usize {
     let strategy = input
         .lines()
         .map(|line| {
-            let choices: Vec<RPSChoices> = line
-                .split(' ')
-                .map(|choice| choice.parse().unwrap())
+            let game: Vec<RPSChoices> = line
+                .split(" ")
+                .map(|choice| choice.parse::<RPSChoices>().unwrap())
                 .collect();
-            {
-                let mut outcome = 0;
-                if choices[0].beats() == choices[1] {
-                    outcome = 0
-                } else if choices[1].beats() == choices[0] {
-                    outcome = 6
-                } else if choices[0].beats() == choices[1].beats() {
-                    outcome = 3
-                }
-                outcome + choices[1] as usize
+
+            if game[0].beats() == game[1] {
+                game[1] as usize + 0
+            } else if game[1].beats() == game[0] {
+                game[1] as usize + 6
+            } else {
+                game[1] as usize + 3
             }
         })
         .sum();
     strategy
 }
 
-fn part2(_input: &str) -> usize {
-    0
+fn part2(input: &str) -> usize {
+    let strategy = input
+        .lines()
+        .map(|line| {
+            let res = line.split(' ')[1].parse::<Outcomes>().unwrap();
+            let opponent = line.split(' ')[0].parse::<RPSChoices>().unwrap();
+
+            choice = match res {
+                Outcomes::Win => {
+                    match opponent {
+                        RPSChoices::Rock => RPSChoices::Paper,
+                        RPSChoices::Paper => RPSChoices::Scissors,
+                        RPSChoices::Scissors => RPSChoices::Rock,
+                    }
+                },
+                Outcomes::Lose {
+                    match opponent {
+                        RPSChoices::Rock => RPSChoices::Scissors,
+                        RPSChoices::Paper => RPSChoices::Rock,
+                        RPSChoices::Scissors => RPSChoices::Paper,
+                    }
+                },
+                Outcomes::Draw {
+                    opponent
+                }
+
+            }
+        })
+        .sum();
+    strategy
 }
 
 #[cfg(test)]
@@ -74,24 +118,24 @@ fn part2(_input: &str) -> usize {
 mod test {
     use super::*;
     const INPUT: &str = "A Y
-    B X
-    C Z";
+B X
+C Z";
 
     // A - Opponent Rock
     // B - Opponent Paper
     // C - Opponent Scissors
 
-    // Y - My Paper
-    // X - My Rock
-    // Z - My Scissors
+    // Y - My Paper || Must Draw
+    // X - My Rock || Must lose
+    // Z - My Scissors || Must Win
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(INPUT), 0);
+        assert_eq!(part1(INPUT), 15);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT), 0);
+        assert_eq!(part2(INPUT), 12);
     }
 }
